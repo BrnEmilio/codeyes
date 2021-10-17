aaa8c32163ff90edda62b931c5abebaac18d5cf2
Bruno Emílio <36413641+BrnEmilio@users.noreply.github.com>
2021-10-17 15:59:04 -0300

    v0
    initial

---
+++ Models/CreatePost.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace codeyes.msc.each.Models
+{
+    public class CreatePost
+    {
+        public string ImageCaption { set; get; }
+        public IFormFile MyImage { set; get; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public System.DateTime DateTime { set; get; }
+    }
+}
